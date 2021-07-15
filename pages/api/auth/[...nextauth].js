d477b0243cec1725600ed9998095901626d8abaa
Praveen Bisht <prvnbist@gmail.com>
2021-07-16 01:34:50 +0530

    fix: reject self signed ssl

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,5 +8,15 @@ export default NextAuth({
          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       }),
    ],
-   database: process.env.DATABASE_URL,
+   database: {
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      ssl: {
+         rejectUnauthorized: false,
+      },
+   },
 })
