a756e559da856e3be0dd350a06de04be328f3253
Praveen Bisht <prvnbist@gmail.com>
2021-07-17 16:38:43 +0530

    feat(component): Toggle

---
+++ components/toggle/index.tsx
@@ -0,0 +1,27 @@
+interface IToggle {
+   id: string
+   is_active: boolean
+   on_change: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export const Toggle = ({ id, is_active, on_change }: IToggle) => {
+   return (
+      <>
+         <input
+            id={id}
+            type="checkbox"
+            checked={is_active}
+            onChange={on_change}
+            className="react-switch-checkbox"
+         />
+         <label
+            htmlFor={id}
+            className="react-switch-label"
+            title={is_active ? 'Hide' : 'Show'}
+            style={{ background: is_active && '#06D6A0' }}
+         >
+            <span className="react-switch-button" />
+         </label>
+      </>
+   )
+}
